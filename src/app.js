2a691bab085b0b2ec99c4faa8e4955bd4ea2bea5
Navarroseb <sebastnavarro@gmail.com>
2022-01-30 17:46:36 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 /* eslint-disable */
-import "bootstrap";
+/* import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico"; */
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 };
